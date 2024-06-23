bb099d7135c610839448ed75aae5408031dbf21c
Thiraj Pramodya <thirajpramodya@Thirajs-MacBook-Pro.local>
2024-06-23 23:22:47 +0530

    Singleton Pattern

---
+++ singleton.js
@@ -0,0 +1,27 @@
+let counter =0;
+
+class Counter{
+    getInstance(){
+        return this;
+    }
+
+    getCounter(){
+        return counter;
+    }
+    incrementCounter(){
+        return ++counter;
+
+    }
+    decrementCounter(){
+        return --counter;
+    }
+
+
+}
+
+const counter1 = new Counter;
+const counter2 = new Counter;
+
+console.log(counter1.getInstance===counter2.getInstance)
+counter1.incrementCounter;
+console.log(counter1.getCounter===counter2.getCounter)
